6c193fdfc56b30766124dc1d335c756ecde5fd22
Tim Turvey <tturvey@hotmail.co.uk>
2022-01-31 14:05:58 +0000

    Array of mines from a preset number of mines

--- public/script.js
+++ public/script.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
-    let width = 10;
     let length = 10;
+    let width = 10;
+    let squaresAmount = length * width;
     let squares = [];
+    let mines = 10;
   
-    // Creates the game board
+    // Creates the game board.
     function createBoard() {
+    
+      // Putting mines on the board.
+      // Making an array the size of the amount of mines on the board.
+      // Fills each array index with a string 'mine'.
+      const mineArray = Array(mines).fill('mine');
+      console.log(mineArray);
+
+
+
       for (let i=0 ; i < length * width; i++){
         const square = document.createElement('div');
         square.setAttribute('id', i);
